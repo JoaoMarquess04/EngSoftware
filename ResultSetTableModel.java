3378b1b86b89b3c8723b62f6c3dde080dafa5d9b
Joao Pinheiro <105454041+JoaoMarquess04@users.noreply.github.com>
2024-09-13 15:35:27 -0300

    Add files via upload

---
+++ ResultSetTableModel.java
@@ -0,0 +1,54 @@
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+
+import javax.swing.table.AbstractTableModel;
+
+public class ResultSetTableModel extends AbstractTableModel {
+    private ResultSet resultSet;
+    private ResultSetMetaData metaData;
+    private int rowCount;
+    private int columnCount;
+
+    public ResultSetTableModel(ResultSet resultSet) throws SQLException {
+        this.resultSet = resultSet;
+        this.metaData = resultSet.getMetaData();
+        this.columnCount = metaData.getColumnCount();
+        
+        // Count rows
+        resultSet.last();
+        this.rowCount = resultSet.getRow();
+        resultSet.beforeFirst();
+    }
+
+    @Override
+    public int getRowCount() {
+        return rowCount;
+    }
+
+    @Override
+    public int getColumnCount() {
+        return columnCount;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        try {
+            resultSet.absolute(rowIndex + 1);
+            return resultSet.getObject(columnIndex + 1);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        try {
+            return metaData.getColumnName(column + 1);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+}
